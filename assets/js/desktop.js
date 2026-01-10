44a381cb606a0350fbaafa5aeca72c42096f4003
Raviolz <gi.ravioli@gmail.com>
2026-01-10 18:31:08 +0100

    TabletRes-Ok

--- assets/js/desktop.js
+++ assets/js/desktop.js
@@ -1,12 +1,21 @@
 const navBar = document.querySelector("nav");
 const navButton = document.querySelector("nav button");
 
-const treshold = 400;
+const tresholdDesktop = 400;
+const tresholdTablet = 400;
 
 window.addEventListener("scroll", () => {
   const scrollPosition = window.scrollY;
 
-  if (scrollPosition > treshold) {
+  let activeTreshold;
+
+  if (window.innerWidth <= 1200) {
+    activeTreshold = tresholdTablet;
+  } else {
+    activeTreshold = tresholdDesktop;
+  }
+
+  if (scrollPosition > activeTreshold) {
     navBar.classList.add("scrolled");
   } else {
     navBar.classList.remove("scrolled");
